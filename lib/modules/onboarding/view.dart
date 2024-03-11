55cb106d48510db1842797fc208c1e1567c2040f
bens1001 <abdelhamid.bensalah.35@gmail.com>
2024-03-11 14:38:24 +0100

    make onboarding screen

--- lib/modules/onboarding/view.dart
+++ lib/modules/onboarding/view.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
+import 'package:get/get.dart';
+import 'package:medical_app_ui/modules/home/view.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 250, 250, 250),
       body: Stack(
         children: [
           Image.asset(
@@ -14,36 +16,70 @@ class OnboardingScreen extends StatelessWidget {
             height: double.infinity,
             width: double.infinity,
             alignment: Alignment.center,
-            color: Color.fromARGB(255, 252, 254, 255),
+            color: const Color.fromARGB(255, 252, 254, 255),
           ),
           Column(
             mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Image.asset(
                 'assets/images/doctor-1.png',
                 height: 650,
                 width: 650,
               ),
-              const SizedBox(height: 20),
-              Text(
-                'All specialists in one app',
-                style: Theme.of(context).textTheme.headline5,
-                textAlign: TextAlign.start,
+              const Padding(
+                padding: EdgeInsets.only(left: 20, top: 20),
+                child: Text(
+                  'All specialists in one app',
+                  style: TextStyle(
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                    color: Color.fromARGB(255, 50, 49, 58),
+                  ),
+                  textAlign: TextAlign.start,
+                ),
               ),
-              const SizedBox(height: 20),
-              Text(
-                'Find your doctor and make an appointment with one tap.',
-                style: Theme.of(context).textTheme.bodyText1,
-                textAlign: TextAlign.start,
+              const Padding(
+                padding: EdgeInsets.only(left: 20, top: 15, bottom: 20),
+                child: Text(
+                  'Find your doctor and make an\n appointment with one tap.',
+                  style: TextStyle(
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.normal,
+                    fontSize: 17,
+                    color: Color.fromARGB(255, 50, 49, 58),
+                  ),
+                  textAlign: TextAlign.start,
+                ),
               ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Get.toNamed('/home');
-                },
-                child: const Text('Get Started'),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 35),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 138, 134, 226),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      minimumSize: const Size(double.infinity, 50),
+                    ),
+                    child: const Text(
+                      'Get Started',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    onPressed: () {
+                      Get.to(() => HomeScreen());
+                    },
+                  ),
+                ),
               ),
-              const SizedBox(height: 20),
             ],
           ),
         ],
